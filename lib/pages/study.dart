e122dfaba48247137bbe3340f69ed465121b861f
胡朋（小莫） <xiaomo@xiaomo.info>
2019-08-24 16:21:41 +0900

    消除警告

--- lib/pages/study.dart
+++ lib/pages/study.dart
@@ -3,13 +3,11 @@ import 'package:flutter_study_app/components/MyAppBar.dart';
 
 class StudyScreen extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => TopTabPage2State();
+  State<StatefulWidget> createState() => StudyState();
 }
 
-class TopTabPage2State extends State<StudyScreen>
+class StudyState extends State<StudyScreen>
     with SingleTickerProviderStateMixin {
-  TabController tabController;
-  List<Widget> tabs = new List();
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +22,6 @@ class TopTabPage2State extends State<StudyScreen>
           ],
         ));
   }
-
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
 }
 
 class HeadNavigator extends StatelessWidget {
@@ -40,67 +32,74 @@ class HeadNavigator extends StatelessWidget {
       children: <Widget>[
         Container(
             child: Center(
-              child: FlatButton(
-                child: Text('库'),
-                onPressed: () => {print('库')},
-              ),
-            )),
+          child: FlatButton(
+            child: Text('库'),
+            onPressed: () => {print('库')},
+          ),
+        )),
         Container(
             child: Center(
-              child: FlatButton(
-                child: Text('Dart'),
-                onPressed: () => {print('Dart')},
-              ),
-            )),
+          child: FlatButton(
+            child: Text('Dart'),
+            onPressed: () => {print('Dart')},
+          ),
+        )),
         Container(
             child: Center(
-              child: FlatButton(
-                child: Text('博客'),
-                onPressed: () => {print('博客')},
-              ),
-            )),     Container(
+          child: FlatButton(
+            child: Text('博客'),
+            onPressed: () => {print('博客')},
+          ),
+        )),
+        Container(
             child: Center(
-              child: FlatButton(
-                child: Text('视频'),
-                onPressed: () => {print('视频')},
-              ),
-            )),     Container(
+          child: FlatButton(
+            child: Text('视频'),
+            onPressed: () => {print('视频')},
+          ),
+        )),
+        Container(
             child: Center(
-              child: FlatButton(
-                child: Text('工具'),
-                onPressed: () => {print('工具')},
-              ),
-            )),     Container(
+          child: FlatButton(
+            child: Text('工具'),
+            onPressed: () => {print('工具')},
+          ),
+        )),
+        Container(
             child: Center(
-              child: FlatButton(
-                child: Text('社区'),
-                onPressed: () => {print('社区')},
-              ),
-            )),     Container(
+          child: FlatButton(
+            child: Text('社区'),
+            onPressed: () => {print('社区')},
+          ),
+        )),
+        Container(
             child: Center(
-              child: FlatButton(
-                child: Text('项目'),
-                onPressed: () => {print('开源项目')},
-              ),
-            )),     Container(
+          child: FlatButton(
+            child: Text('项目'),
+            onPressed: () => {print('开源项目')},
+          ),
+        )),
+        Container(
             child: Center(
-              child: FlatButton(
-                child: Text('游戏'),
-                onPressed: () => {print('游戏')},
-              ),
-            )),     Container(
+          child: FlatButton(
+            child: Text('游戏'),
+            onPressed: () => {print('游戏')},
+          ),
+        )),
+        Container(
             child: Center(
-              child: FlatButton(
-                child: Text('游戏引擎'),
-                onPressed: () => {print('游戏引擎')},
-              ),
-            )),     Container(
+          child: FlatButton(
+            child: Text('游戏引擎'),
+            onPressed: () => {print('游戏引擎')},
+          ),
+        )),
+        Container(
             child: Center(
-              child: FlatButton(
-                child: Text('其他'),
-                onPressed: () => {print('其他')},
-              ),
-            )),
+          child: FlatButton(
+            child: Text('其他'),
+            onPressed: () => {print('其他')},
+          ),
+        )),
       ],
     );
   }
