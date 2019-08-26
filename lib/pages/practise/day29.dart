7a5841d02b7e57e3af46d095778e618f4b393ac0
kohou <hoh_ko@ricoh-japan.co.jp>
2019-08-26 20:36:50 +0900

    每日一练

---
+++ lib/pages/practise/day29.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Day29Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('这是博客')),
+    );
+  }
+}
