ddc3af2ac7e7dfab97b538ebb75e9ce59a62029d
kohou <hoh_ko@ricoh-japan.co.jp>
2019-08-30 13:29:35 +0900

    chat detail

---
+++ lib/pages/chat/chat_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatDetailScreen extends StatefulWidget {
+  @override
+  _ChatDetailScreenState createState() => _ChatDetailScreenState();
+}
+
+class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Text('chat detail')
+    );
+  }
+}
