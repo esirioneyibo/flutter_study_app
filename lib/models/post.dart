b59a832f7f85c42a6a12f41ef5a9e268620728a3
胡朋（小莫） <xiaomo@xiaomo.info>
2019-09-01 20:48:26 +0900

    修改post布局

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,45 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 class Post {
+  // 唯一id
+  String id = Uuid().v1();
+
+  // 文章标题
   String title;
-  String subTitle;
+
+  // 文章内容
+  String content;
+
+  // 作者名字
+  String author;
+
+  // 作者头像
   IconData icon;
 
-  Post(this.title, this.subTitle, this.icon);
+  // 标签
+  String tag;
 
-  static List<Post> news = [
-    Post('动态1', '内容', Icons.save),
-    Post('动态2', '内容', Icons.data_usage),
-    Post('动态3', '内容', Icons.access_time),
-    Post('动态4', '内容', Icons.delete_forever),
-    Post('动态5', '内容', Icons.euro_symbol),
-    Post('动态6', '内容', Icons.eject),
-    Post('动态7', '内容', Icons.favorite),
-    Post('动态8', '内容', Icons.delete_outline),
-    Post('动态9', '内容', Icons.account_box),
-  ];
+  // 时间
+  String dateTime;
+
+  // 评论
+  List<Comment> comments;
+
+  Post(this.title, this.content, this.author, this.icon, this.tag,
+      this.dateTime, this.comments);
 
   static List<Post> posts = [
-    Post('这是标题1', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.monetization_on),
-    Post('这是标题2', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.satellite),
-    Post('这是标题3', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.access_alarm),
-    Post('这是标题4', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.email),
-    Post('这是标题5', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.backup),
-    Post('这是标题6', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.gamepad),
-    Post('这是标题7', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.backspace),
-    Post('这是标题8', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.filter),
-    Post('这是标题9', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.adjust),
-    Post('这是标题10', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.image_aspect_ratio),
-    Post('这是标题11', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.hdr_off),
-    Post('这是标题12', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.keyboard_arrow_down),
-    Post('这是标题13', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.monetization_on),
-    Post('这是标题14', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.label_outline),
-    Post('这是标题15', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.nature_people),
-    Post('这是标题16', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.offline_pin),
-    Post('这是标题17', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.palette),
-    Post('这是标题18', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.question_answer),
-    Post(
-        '这是标题19', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.radio_button_checked),
-    Post('这是标题20', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', Icons.save),
+    Post('这是标题1', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', '小莫', Icons.people,
+        'flutter', '${DateTime.now().hour}:${DateTime.now().minute}', List()),
+    Post('这是标题1', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', '小莫', Icons.print,
+        'flutter', '${DateTime.now().hour}:${DateTime.now().minute}', List()),
+    Post('这是标题1', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', '小莫', Icons.hourglass_empty,
+        'flutter', '${DateTime.now().hour}:${DateTime.now().minute}', List()),
+    Post('这是标题1', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', '小莫', Icons.adb,
+        'flutter', '${DateTime.now().hour}:${DateTime.now().minute}', List()),
+    Post('这是标题1', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', '小莫', Icons.home,
+        'flutter', '${DateTime.now().hour}:${DateTime.now().minute}', List()),
+    Post('这是标题1', '假装有内容假装有内容假装有内容假装有内容假装有内容假装有内容', '小莫', Icons.email,
+        'flutter', '${DateTime.now().hour}:${DateTime.now().minute}', List()),
   ];
+}
+
+class Comment {
+  String id = Uuid().v1();
+  String author;
+  IconData icon;
+  String content;
+  DateTime time;
 }
--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -12,26 +12,87 @@ class ChatScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: MyAppBar(context, MyLocalizations.of(context).chat),
-      body: Center(
+      body: Container(
+        color: Colors.grey,
         child: ListView.builder(
             itemCount: posts.length,
             itemBuilder: (context, index) {
-              return Column(
+              // 左侧作者信息
+              var authInfo = Row(
                 children: <Widget>[
-                  ListTile(
-                    title: Text(posts[index].title),
-                    leading: Icon(posts[index].icon),
-                    subtitle: Text(
-                      posts[index].subTitle,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    onTap: () {
-                      RouterUtil.push(context, ChatDetailScreen());
-                    },
-                  ),
-                  Divider()
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        posts[index].icon,
+                        size: 40,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(posts[index].author),
+                          Text(
+                            posts[index].dateTime,
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
                 ],
+              );
+
+              // 右侧评论数量
+              var commentsSize = Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    padding: EdgeInsets.all(5),
+                    color: Colors.grey,
+                    child: Text(
+                      posts[index].tag,
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Icon(
+                      Icons.message,
+                      color: Colors.grey,
+                      size: 15,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(5),
+                    child: Text(
+                      posts[index].comments.length.toString(),
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  )
+                ],
+              );
+              return InkWell(
+                onTap: (){
+                  RouterUtil.push(context, ChatDetailScreen());
+                },
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  padding: EdgeInsets.all(15),
+                  color: Colors.white,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Expanded(flex: 2, child: authInfo), // 左侧信息
+                          Expanded(flex: 1, child: commentsSize), // 右侧评论数
+//                        Text("这是上面")
+                        ],
+                      ),
+                      Container(
+                          padding: EdgeInsets.only(top: 15),
+                          alignment: Alignment.centerLeft,
+                          child: Text(posts[index].content,))
+                    ],
+                  ),
+                ),
               );
             }),
       ),
