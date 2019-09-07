bb7f2948640465bfbc5c6a9526542bdaf40ad0b6
胡朋（小莫） <xiaomo@xiaomo.info>
2019-09-08 01:42:47 +0900

    sign

--- lib/pages/practise_screen.dart
+++ lib/pages/practise_screen.dart
@@ -1,32 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study_app/components/choice_card.dart';
-import 'package:flutter_study_app/components/my_app_bar.dart';
-import 'package:flutter_study_app/i10n/localization_intl.dart';
-import 'package:flutter_study_app/vo/choice_vo.dart';
 
-class PractiseScreen extends StatelessWidget {
+class Item {
+  Item({
+    this.expandedValue,
+    this.headerValue,
+    this.isExpanded = false,
+  });
+
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+}
+
+List<Item> generateItems(int numberOfItems) {
+  return List.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: '第${index + 1}天',
+      expandedValue: 'This is item number $index',
+    );
+  });
+}
+
+class PractiseScreen extends StatefulWidget {
+  PractiseScreen({Key key}) : super(key: key);
+
+  @override
+  _PractiseScreenState createState() => _PractiseScreenState();
+}
+
+class _PractiseScreenState extends State<PractiseScreen> {
+  List<Item> _data = generateItems(30);
+
   @override
   Widget build(BuildContext context) {
-    /// 工具列表
-
-    var toolsSection = GridView.count(
-      crossAxisCount: 4,
-      shrinkWrap: true,
-      children:
-          List.generate(choices.length, (index) {
-        return Center(
-          child:
-              ChoiceCard(choice: choices[index]),
-        );
-      }),
-    );
-
     return Scaffold(
-        appBar: MyAppBar(
-            title: MyLocalizations.of(context)
-                .study),
-        body: Container(
-          child: toolsSection,
-        ));
+      appBar: AppBar(
+        title: Text('每日一练'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: _buildPanel(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPanel() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text(item.headerValue),
+            );
+          },
+          body: ListTile(
+              title: Text(item.expandedValue),
+              subtitle: Text(' To delete this panel, tap the trash can icon'),
+              trailing: Icon(Icons.delete),
+              onTap: () {
+                setState(() {
+                  _data.removeWhere((currentItem) => item == currentItem);
+                });
+              }),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
+    );
   }
 }
