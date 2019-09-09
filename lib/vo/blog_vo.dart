9d9390b86db24d1d2d72d6019b4a681e0acebfc4
kohou <hoh_ko@ricoh-japan.co.jp>
2019-09-09 18:12:20 +0900

    blog

---
+++ lib/vo/blog_vo.dart
@@ -0,0 +1,68 @@
+class BlogVo {
+  String title;
+  String desc;
+  String url;
+
+  BlogVo(
+      {this.title,
+      this.desc,
+      this.url});
+}
+
+final List<BlogVo> blogs = [
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+  BlogVo(
+      title: 'blog',
+      desc: 'this is an '
+          'introduction for blog',
+      url: 'https://github.com/houko/blog'),
+];
