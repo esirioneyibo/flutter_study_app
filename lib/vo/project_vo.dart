0fcea0af9a4f5ba524cd9ebe87793cce95234136
kohou <hoh_ko@ricoh-japan.co.jp>
2019-09-09 18:07:23 +0900

    projects

---
+++ lib/vo/project_vo.dart
@@ -0,0 +1,96 @@
+class ProjectVo {
+  String projectName;
+  String desc;
+  String language;
+  String star;
+  String fork;
+  String url;
+
+  ProjectVo(
+      {this.projectName,
+      this.desc,
+      this.language,
+      this.star,
+      this.fork,
+      this.url});
+}
+
+final List<ProjectVo> projects = [
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+  ProjectVo(
+      projectName: 'angular',
+      desc: 'this is an '
+          'project for angular',
+      star: '4221',
+      fork: '123',
+      url: 'https://github.com/houko/Angular'),
+];
