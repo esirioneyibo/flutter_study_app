2dd8a9bb4270378d24f3d7fd468d8c2aeb1f3fae
kohou <hoh_ko@ricoh-japan.co.jp>
2019-09-10 16:41:18 +0900

    web view

--- lib/pages/tools/study/wiki_screen.dart
+++ lib/pages/tools/study/wiki_screen.dart
@@ -1,9 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_study_app/components/return_bar.dart';
 import 'package:flutter_study_app/i10n/localization_intl.dart';
 import 'package:flutter_study_app/utils/navigator_util.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WikiScreen extends StatelessWidget {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -13,7 +19,30 @@ class WikiScreen extends StatelessWidget {
       child: Scaffold(
         appBar: ReturnBar(
             MyLocalizations.of(context).returnButton),
-        body: Center(child: Text('wiki')),
+        body: WebView(
+          initialUrl: 'https://www.wikipedia'
+              '.org/',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController controller) {
+            _controller.complete(controller);
+            controller.evaluateJavascript('''
+        inputs = document.querySelectorAll("input[type=text]");
+        inputs.forEach(function(inp) {
+            let finalInput = inp;
+            finalInput.addEventListener("focus", function() {
+                console.log('focus');
+                input = finalInput;
+                InputValue.postMessage('');
+                Focus.postMessage('focus');
+           });
+           finalInput.addEventListener("focusout", function() {
+               console.log('unfocus');
+               Focus.postMessage('focusout');
+           });
+      });
+''');
+          },
+        ),
       ),
     );
   }
